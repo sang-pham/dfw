8c4e9ea54361c1c4dfce8871284068569e517b3e
sang-pham <sangp41@ghtk.co>
2023-03-27 16:05:50 +0700

    update

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 const { isUserConfigChange, getUserCache, getToken } = require('./cache/user.cache')
-const config = require('../../config.json')
+// const config = require('../../config.json')
 const firewallConfig = require('../config/routers')
 const constant = require('./constant')
 const bcrypt = require('bcrypt')
@@ -10,43 +10,43 @@ const firewalls = firewallConfig.get('routers')
 //   {name: 'fw1', ip: '192.168.191.1', port: 5000, network: '192.168.191.0/24'}
 // ]
 
-const checkEnv = () => {
-  const AUTH_URL = config['auth_url']
-  const USERNAME = config['username']
-  const USERID = config['user_id']
-  const DOMAIN = config['domain']
-  const PASSWORD = config['password']
-  const PROJECT_ID = config['project_id']
-  if (!AUTH_URL) {
-    throw new Error('You must specify the AUTH_URL variable')
-  }
-  if (!PASSWORD) {
-    throw new Error('You must specify your password')
-  }
-  if ((!USERID || !PROJECT_ID) && (!USERNAME || !DOMAIN)) {
-    throw new Error('You must specify user ID and project ID or user name and domain')
-  }
-}
+// const checkEnv = () => {
+//   const AUTH_URL = config['auth_url']
+//   const USERNAME = config['username']
+//   const USERID = config['user_id']
+//   const DOMAIN = config['domain']
+//   const PASSWORD = config['password']
+//   const PROJECT_ID = config['project_id']
+//   if (!AUTH_URL) {
+//     throw new Error('You must specify the AUTH_URL variable')
+//   }
+//   if (!PASSWORD) {
+//     throw new Error('You must specify your password')
+//   }
+//   if ((!USERID || !PROJECT_ID) && (!USERNAME || !DOMAIN)) {
+//     throw new Error('You must specify user ID and project ID or user name and domain')
+//   }
+// }
 
-const checkCredentials = async () => {
-  try {
-    checkEnv()
-    let obj = {
-      username: config['username'],
-      userId: config['user_id'],
-      password: config['password'],
-      domain: config['domain'],
-      projectId: config['project_id']
-    }
-    let token = await getToken()
-    let isConfigChange = await isUserConfigChange(obj)
-    let userCache = await getUserCache()
-    if (isConfigChange|| !userCache || !token) return false
-    return true
-  } catch (error) {
-    throw error
-  }
-}
+// const checkCredentials = async () => {
+//   try {
+//     checkEnv()
+//     let obj = {
+//       username: config['username'],
+//       userId: config['user_id'],
+//       password: config['password'],
+//       domain: config['domain'],
+//       projectId: config['project_id']
+//     }
+//     let token = await getToken()
+//     let isConfigChange = await isUserConfigChange(obj)
+//     let userCache = await getUserCache()
+//     if (isConfigChange|| !userCache || !token) return false
+//     return true
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
 const checkValidNetwork = network => {
   if (!network) return false
@@ -661,8 +661,8 @@ const randomKey = async () => {
 }
 
 module.exports = {
-  checkCredentials,
-  checkEnv,
+  // checkCredentials,
+  // checkEnv,
   checkValidNetwork,
   autoIdentifyFirewalls,
   checkIpInNetwork,
