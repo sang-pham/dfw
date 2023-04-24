fde416679cab8c0796779c46c8244b988a86c4a7
sang-pham <sangp41@ghtk.co>
2023-04-24 21:12:03 +0700

    update

--- src/helper/formatRules.js
+++ src/helper/formatRules.js
@@ -1,9 +1,9 @@
 const formatRules = (data, options = {}) => {
   let withLineNumber = options['lineNumbers']
   if (withLineNumber) {
-    console.log('num\ttarget\t\tprot\t\tsource\t\t\tdestination')
+    console.log('num\ttarget\t\tprot\t\tsource\t\t\tdestination\t\tin\t\tout')
   } else {
-    console.log('target\t\tprot\t\tsource\t\t\tdestination')
+    console.log('target\t\tprot\t\tsource\t\t\tdestination\t\tin\t\tout')
   }
   let i = 1
   for (const item of data) {
@@ -21,16 +21,18 @@ const formatRules = (data, options = {}) => {
     res += `${item.protocol || 'all'}\t\t`
     res += `${item.src || 'anywhere'}\t\t`
     res += `${item.dst || 'anywhere'}\t\t`
+    res += `${item['in-interface'] || '*'}\t\t`
+    res += `${item['out-interface'] || '*'}\t\t`
     if (Object.keys(item.target) == 'REJECT') {
       const rejectMessObj = item.target['REJECT']
       if (Object.keys(rejectMessObj).length) {
         for (const key in rejectMessObj) {
-          res += `${key} ${rejectMessObj[key]}`
+          res += `${key} ${rejectMessObj[key]}\t`
         }
       }
     }
     if (item.conntrack && item.conntrack.ctstate) {
-      res += item.conntrack.ctstate
+      res += `${item.conntrack.ctstate}\t`
     }
     let protocol = item.protocol
     if (item[protocol] && Object.keys(item[protocol]).length) {
